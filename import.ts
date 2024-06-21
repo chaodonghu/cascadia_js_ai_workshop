539dba945acbac9eb9f5a278dead290440b7d2bd
chaodonghu <dojostan@gmail.com>
2024-06-21 11:43:06 -0700

    add gitignore file for repo

--- import.ts
+++ import.ts
@@ -5,22 +5,21 @@ async function sendToAstra() {
     tweets.data.search_by_raw_query.search_timeline.timeline.instructions[0]
       .entries;
 
-  console.log(
-    arrayOfTweets.map((t) => {
-      const id =
-        t.content.itemContent?.tweet_results.result.legacy.id_str ?? "";
-      const date =
-        t.content.itemContent?.tweet_results.result.legacy.created_at ?? "";
-      const text =
-        t.content.itemContent?.tweet_results.result.legacy.full_text ?? "";
+  const dataToImport = arrayOfTweets.map((t) => {
+    const id = t.content.itemContent?.tweet_results.result.legacy.id_str ?? "";
+    const date =
+      t.content.itemContent?.tweet_results.result.legacy.created_at ?? "";
+    const text =
+      t.content.itemContent?.tweet_results.result.legacy.full_text ?? "";
 
-      return {
-        id,
-        date,
-        text,
-      };
-    })
-  );
+    return {
+      date,
+      id,
+      text,
+      vectorize: text,
+    };
+  });
+  
 }
 
 sendToAstra();
